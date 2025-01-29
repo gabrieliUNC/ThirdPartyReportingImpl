03fe4acb9df996bd41fa03dbdc8490e68d2395bc
Gabriel Schell <gabrieli@ad.unc.edu>
2025-01-28 20:31:41 -0500

    Update basic_mod_setup.rs

--- third_party_reporting/benches/basic_mod_setup.rs
+++ third_party_reporting/benches/basic_mod_setup.rs
@@ -1,21 +1,19 @@
 use criterion::*;
 use third_party_reporting::lib_basic as basic;
-
-pub fn setup_mod(pk_reg: &Option<Vec<u8>>, num_moderators: usize) {
-    for _i in 0..num_moderators {
-        basic::Moderator::new(&pk_reg);
-    }
-}
+use third_party_reporting::lib_common::*;
 
 
 pub fn bench_setup_mod(c: &mut Criterion) {
     // One time setup to generate platform needed for mod setup
-    let platform = basic::test_basic_setup_platform();
+    let mut platforms: Vec<basic::Platform> = Vec::new();
+    for _i in 0..MOD_SCALE.len() {
+        platforms.push(basic::Platform::new());
+    }
 
     let mut group = c.benchmark_group("basic.setup_mod()");
-    for num_moderators in [1, 2, 4].iter() {
-        group.bench_with_input(BenchmarkId::from_parameter(num_moderators), num_moderators, |b, &num_moderators| {
-            b.iter(|| setup_mod(&platform.k_reg, num_moderators))
+    for (i, num_moderators) in MOD_SCALE.iter().enumerate() {
+        group.bench_with_input(format!("basic.setup_mod() with {} moderators", num_moderators), num_moderators, |b, &num_moderators| {
+            b.iter(|| basic::test_basic_setup_mod(&mut platforms[i], num_moderators))
         });
     }
     group.finish();
