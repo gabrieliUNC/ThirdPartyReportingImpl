b2ace075bb8287eae635999404f8824819822313
Gabriel Schell <gabrieli@ad.unc.edu>
2025-03-31 12:39:02 -0400

    Script for Main Table generation

--- third_party_reporting/target/script.py
+++ third_party_reporting/target/script.py
@@ -1,18 +1,42 @@
 import os
 import json
+import pandas as pd
+
+''' Script to make the running time table for Third Party Reporting Schemes '''
 
 path = "criterion"
 dirs = os.listdir(path)
 data = {}
+schemes = ['basic', 'mod-priv', 'const-mod-priv']
+benches = ['send', 'process', 'read', 'report', 'moderate']
 
 for dir in dirs:
-    if dir.endswith("()"):
+    scheme = dir[:dir.find('.')]
+    bench = dir[dir.find('.') + 1:-2]
+
+    if scheme in schemes:
         sub_dirs = os.listdir(path + '/' + dir)
         for sub_dir in sub_dirs:
-            if sub_dir != 'report':
-                fname = path + '/' + dir + '/' + sub_dir + '/base/estimates.json'
-                f = open(fname, 'r')
-                data[str(sub_dir)] = f.read()
+            fname = path + '/' + dir + '/' + sub_dir + '/base/estimates.json'
+            f = open(fname, 'r')
+            cur = json.loads(f.read())
+            f.close()
 
-js = json.dumps(data)
-print(js)
+            if bench in benches:
+                if bench not in data:
+                    data[bench] = {}
+        
+                if scheme in schemes:
+                    data[bench][scheme] = int(cur['mean']['point_estimate']) / 1e3
+
+
+print('DataFrame output')
+df = pd.DataFrame(data)
+df = df[benches]
+df = df.reindex(['basic', 'mod-priv', 'const-mod-priv'])
+print(df)
+print()
+
+
+#print('CSV output')
+#print(df.to_csv())
