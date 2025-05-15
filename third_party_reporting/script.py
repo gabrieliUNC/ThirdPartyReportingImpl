48dcba002bb2964bdf61c1e19bfd36f980571a5f
Gabriel Schell <gabrieli@ad.unc.edu>
2025-05-15 19:34:51 -0400

    Update script.py

--- third_party_reporting/script.py
+++ third_party_reporting/script.py
@@ -9,7 +9,7 @@ dirs = os.listdir(path)
 data = {}
 schemes = ['basic', 'mod-priv', 'const-mod-priv', 'plain']
 benches = ['send', 'process', 'read', 'report', 'moderate']
-NUM_MODS = '64'
+NUM_MODS = ['2 ', '64 ']
 
 for dir in dirs:
     scheme = dir[:dir.find('.')]
@@ -18,7 +18,14 @@ for dir in dirs:
     if scheme in schemes:
         sub_dirs = os.listdir(path + '/' + dir)
         for sub_dir in sub_dirs:
-            if sub_dir == 'report' or ('plain' not in sub_dir and NUM_MODS not in sub_dir):
+            if sub_dir == 'report':
+                continue
+            num_mods = '1 '
+            for num in NUM_MODS:
+                if num in sub_dir:
+                    num_mods = num
+
+            if 'plain' not in sub_dir and num_mods == '1 ':
                 continue
             fname = path + '/' + dir + '/' + sub_dir + '/base/estimates.json'
             f = open(fname, 'r')
@@ -28,15 +35,16 @@ for dir in dirs:
             if bench in benches:
                 if bench not in data:
                     data[bench] = {}
-        
+                
+                title = scheme + ' { ' + num_mods + '}'
                 if scheme in schemes:
-                    data[bench][scheme] = int(cur['mean']['point_estimate']) / 1e3
+                    data[bench][title] = int(cur['mean']['point_estimate']) / 1e3
 
 
 print('DataFrame output')
 df = pd.DataFrame(data)
 df = df[benches]
-df = df.reindex(['basic', 'mod-priv', 'const-mod-priv', 'plain'])
+#df = df.reindex(['basic', 'mod-priv', 'const-mod-priv', 'plain'])
 print(df)
 print()
 
