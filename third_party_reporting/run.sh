c51edd9575437b3fde7ade9d096061cdb3a736eb
Gabriel Schell <gabrieli@ad.unc.edu>
2025-07-30 08:38:17 -0400

    Update scripts

--- third_party_reporting/run.sh
+++ third_party_reporting/run.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-#cargo clean
+cargo clean
 
-#echo "\n\nBuilding All Packages!\n\n"
-#cargo build
+echo "\n\nBuilding All Packages!\n\n"
+cargo build
 
-#echo "\n\nRunning Computation Time Benchmarks!\n\n"
-#cargo bench
+echo "\n\nRunning Computation Time Benchmarks!\n\n"
+cargo bench
 
 echo "\n\nCollecting Data into Computation Time Table!\n\n"
 python3 script.py
@@ -18,7 +18,6 @@ echo "\n\nRunning All schemes and reporting Communication Costs!\n\n"
 cargo run -- --num-moderators 1 --msg-size 100 --test-e2ee --basic --const-priv
 
 # Run Additional communication cost tests for Mod Priv-1
-
 for ((i = 1; i <= 11; ++i)); do
 	cargo run -- --num-moderators $i --mod-priv
 done
